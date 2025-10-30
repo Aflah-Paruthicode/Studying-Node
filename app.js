b4722544d565e25dd277157c2170ac635a34d1da
Aflah Paruthicode <aflutech2@gmail.com>
2025-10-30 12:57:10 +0530

    Module Export & Require

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 require("./imp");
 
+const calculateSum = require('./sum');
+
 let current = "current module";
 console.log(current);
+
+calculateSum(1,9)
+
---
+++ sum.js
@@ -0,0 +1,5 @@
+function calculateSum(num1, num2) {
+  console.log("sum is here : ", num1 + num2);
+}
+
+module.exports = calculateSum
